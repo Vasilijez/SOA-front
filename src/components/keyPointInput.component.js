81a06c51cfa8005545f52fa17445bd8c6ce56b24
Pavle Krstić <pavlekrstic010@gmail.com>
2024-04-24 20:47:24 +0200

    Leaving markers for added keypoints on the map

--- src/components/keyPointInput.component.js
+++ src/components/keyPointInput.component.js
@@ -5,13 +5,13 @@ import { useMapEvents } from 'react-leaflet';
 import { Icon} from 'leaflet';
 
 const KeyPointInput = ({ keyPoint, onChange, onAdd }) => {
-
     const [clickedPosition, setClickedPosition] = useState(null);
-
+    const [keyPoints, setKeyPoints] = useState([]);
+    
     const customIcon = new Icon({
         iconUrl: require("../location-pin.png"),
-        iconSize : [38,38]
-    })
+        iconSize: [38, 38]
+    });
 
     function MyComponent() {
         useMapEvents({
@@ -24,26 +24,36 @@ const KeyPointInput = ({ keyPoint, onChange, onAdd }) => {
         return null;
     }
 
+    const handleAddKeyPoint = () => {
+        if (clickedPosition) {
+            setKeyPoints([...keyPoints, clickedPosition]);
+            setClickedPosition(null); // Reset clicked position after adding
+        }
+    };
+
     return (
         <div>
-            {/* <div>
-                <input type="number" name="longitude" placeholder="Longitude" value={keyPoint.longitude} onChange={onChange} />
-                <input type="number" name="latitude" placeholder="Latitude" value={keyPoint.latitude} onChange={onChange} />
-                <button type="button" onClick={onAdd}>Add Key Point</button>
-            </div> */}
-
             <MapContainer center={[48.8566, 2.3533]} zoom={13}>
                 <TileLayer
                     attribution='https://www.openstreetmap.org/copyright'
                     url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
                 />
                 <MyComponent />
+                {keyPoints.map((point, index) => (
+                    <Marker key={index} icon={customIcon} position={[point.latitude, point.longitude]}>
+                        <Popup>
+                            Latitude: {point.latitude}<br />
+                            Longitude: {point.longitude}
+                        </Popup>
+                    </Marker>
+                ))}
                 {clickedPosition && (
                     <Marker icon={customIcon} position={[clickedPosition.latitude, clickedPosition.longitude]}>
                         <Popup>
                             Latitude: {clickedPosition.latitude}<br />
                             Longitude: {clickedPosition.longitude}
-                            <button type='button' onClick={() => onAdd(clickedPosition.latitude, clickedPosition.longitude)}>Add Key Point</button>                        </Popup>
+                            <button type='button' onClick={() => { onAdd(clickedPosition.latitude, clickedPosition.longitude); handleAddKeyPoint(); }}>Add Key Point</button>
+                        </Popup>
                     </Marker>
                 )}
             </MapContainer>
